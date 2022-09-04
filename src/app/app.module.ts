776672534b231e3f8f85f48a481cb74e9e9f7470
Cesar Velasco Ariza <cesar.ariza.velasco@hotmail.com>
2022-09-03 20:32:09 -0500

    actualizacion del front contruccion de las partes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { GuideComponent } from './components/guide/guide.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { CheckInComponent } from './components/check-in/check-in.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { StartContentComponent } from './components/start-content/start-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CarouselComponent,
+    AboutUsComponent,
+    GuideComponent,
+    QuoteComponent,
+    CheckInComponent,
+    LogInComponent,
+    StartContentComponent
   ],
   imports: [
     BrowserModule,
