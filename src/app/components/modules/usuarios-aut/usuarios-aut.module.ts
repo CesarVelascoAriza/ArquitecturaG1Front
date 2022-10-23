8e1b6c0420065c9ba1bbb36b0885e1e87d24871c
cesar velasco <caacesar.ariza@gmail.com>
2022-10-23 17:40:12 -0500

    cambios

---
+++ src/app/components/modules/usuarios-aut/usuarios-aut.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsuariosAutRoutingModule } from './usuarios-aut-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UsuariosAutRoutingModule
+  ]
+})
+export class UsuariosAutModule { }
